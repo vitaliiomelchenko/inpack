c89705d73e62b100f5e54559c3e6a69b152b8f88
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2021-09-10 14:37:06 +0300

    partners slider

--- js/main.js
+++ js/main.js
@@ -361,29 +361,47 @@ $('.heroBanner__sliderWrapper').slick({
   nextArrow: $('.heroBanner .slick-next'),
 });
 //Partners slider
+
 $('.partners__row').slick({
-  infinite: true,
+  dots: false,
+  infinite: false,
+  speed: 300,
   slidesToShow: 6,
-  slidesToScroll: 1,
+  slidesToScroll: 4,
   prevArrow: $('.partners__content__wrapper .slick-prev'),
   nextArrow: $('.partners__content__wrapper .slick-next'),
   responsive: [
     {
-      breakpoint: 768,
+      breakpoint: 1024,
       settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
         arrows: false,
-        centerMode: true,
-        slidesToShow: 3
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: false
       }
     },
     {
       breakpoint: 480,
       settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
         arrows: false,
-        centerMode: true,
-        slidesToShow: 3
+        dots: false
       }
     }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
   ]
 });
 
