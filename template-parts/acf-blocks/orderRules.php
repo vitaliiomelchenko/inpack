67fb25e33a08bda325a10addcb25240a409ec6fb
KorniienkoIvan <kornienkoivan462@gmail.com>
2021-09-20 16:58:56 +0300

    shipping page responsive

--- template-parts/acf-blocks/orderRules.php
+++ template-parts/acf-blocks/orderRules.php
@@ -2,28 +2,32 @@
     $orderRulesTitle = get_sub_field('orderRulesTitle'); 
 ?>
 <section class="orderRules">
-        <div class="container--md">
-            <?php if ($orderRulesTitle) : ?>
-                    <div class="orderRules__title h2"><?php echo $orderRulesTitle;?></div>
-                <?php endif;?>
+    <div class="container">
+        <?php if ($orderRulesTitle) : ?>
+            <div class="orderRules__title h2"><?php echo $orderRulesTitle;?></div>
+        <?php endif;?>
+        <?php if (have_rows('orderRulesItem') ): ?>
             <ol class="row">
-                <?php if (have_rows('orderRulesItem') ): ?>
-                    <?php while(have_rows('orderRulesItem') ) : the_row();
-                        $orderRulesItemTitle = get_sub_field('orderRulesItemTitle'); 
-                        $orderRulesItemContent = get_sub_field('orderRulesItemContent'); 
-                    ?>
-                <li class=" col-lg-4 orderRules__item">
-                    <div class="orderRules__itemInner" >
-                        <?php if ($orderRulesItemTitle) : ?>
-                            <div class="orderRules__itemTitle"><?php echo $orderRulesItemTitle;?></div>
-                        <?php endif;?>
-                        <?php if ($orderRulesItemContent) : ?>
-                            <div class="orderRules__itemContent"><?php echo $orderRulesItemContent;?></div>
-                        <?php endif;?>
-                    </div>
-                </li>
+                <?php $i = 1; ?>
+                <?php while(have_rows('orderRulesItem') ) : the_row();
+                    $orderRulesItemTitle = get_sub_field('orderRulesItemTitle'); 
+                    $orderRulesItemContent = get_sub_field('orderRulesItemContent');  ?>
+                    <li class=" col-lg-4 orderRules__item">
+                        <div class="item_number">
+                            <?php echo $i; ?>
+                        </div>
+                        <div class="orderRules__itemInner" >
+                            <?php if ($orderRulesItemTitle) : ?>
+                                <div class="orderRules__itemTitle"><?php echo $orderRulesItemTitle;?></div>
+                            <?php endif;?>
+                            <?php if ($orderRulesItemContent) : ?>
+                                <div class="orderRules__itemContent"><?php echo $orderRulesItemContent;?></div>
+                            <?php endif;?>
+                        </div>
+                    </li>
+                    <?php $i++; ?>
                 <?php endwhile;?>
             </ol>
-        </div>
-    </section>
-    <?php endif;?>
+        <?php endif;?>
+    </div>
+</section>
--- template-parts/acf-blocks/shipping.php
+++ template-parts/acf-blocks/shipping.php
@@ -5,40 +5,40 @@
 
 <section class="shipping shipping_section">
     <div class="shipping__content__wrapper">
-    <div class="container--featured titlePseudo--container">
-        <?php if ($shippingTitle) : ?>
-            <div class="shippingTitle h2 text-color-primary"><?php echo $shippingTitle;?></div>
-        <?php endif;?>
-        <?php if ($shippingTitleBack) : ?>
+        <div class="container titlePseudo--container">
+            <?php if ($shippingTitle) : ?>
+                <div class="shippingTitle h2 text-color-primary"><?php echo $shippingTitle;?></div>
+            <?php endif;?>
+            <?php if ($shippingTitleBack) : ?>
                 <div class="titlePseudo shippingPseudo"><?php echo $shippingTitleBack;?></div>
             <?php endif;?>
-        <div class="row shipping__itemRow">
-            <!--  -->
+        </div>
+        <div class="container--featured">
             <?php if (have_rows('shippingItem') ): ?>
-                <?php while(have_rows('shippingItem') ) : the_row();
-                    $shippingItemTitle = get_sub_field('shippingItemTitle'); 
-                    $shippingItemContent = get_sub_field('shippingItemContent'); 
-                    $shippingItemIcon = get_sub_field('shippingItemIcon'); 
-                ?>
-            <div class="col-md-6 shipping__item">
-                <?php if( !empty( $shippingItemIcon ) ): ?>
-                <div class="shipping__itemIcon">
-                    <?php echo file_get_contents(wp_get_original_image_path($shippingItemIcon['id'])); ?>
-                    </div>
-                <?php endif;?>
-                <div class="shipping_itemInner">
-                    <?php if ($shippingItemTitle) : ?>
-                        <div class="shipping__itemTitle"><?php echo $shippingItemTitle;?></div>
-                    <?php endif;?>
-                    <?php if ($shippingItemContent) : ?>
-                        <div class="shipping__itemContent"><?php echo $shippingItemContent;?></div>
-                    <?php endif;?>
+                <div class="row shipping__itemRow">
+                    <?php while(have_rows('shippingItem') ) : the_row();
+                        $shippingItemTitle = get_sub_field('shippingItemTitle'); 
+                        $shippingItemContent = get_sub_field('shippingItemContent'); 
+                        $shippingItemIcon = get_sub_field('shippingItemIcon'); 
+                    ?>
+                        <div class="col-md-6 shipping__item">
+                            <?php if( !empty( $shippingItemIcon ) ): ?>
+                                <div class="shipping__itemIcon">
+                                    <?php echo file_get_contents(wp_get_original_image_path($shippingItemIcon['id'])); ?>
+                                </div>
+                            <?php endif;?>
+                            <div class="shipping_itemInner">
+                                <?php if ($shippingItemTitle) : ?>
+                                    <div class="shipping__itemTitle"><?php echo $shippingItemTitle;?></div>
+                                <?php endif;?>
+                                <?php if ($shippingItemContent) : ?>
+                                    <div class="shipping__itemContent"><?php echo $shippingItemContent;?></div>
+                                <?php endif;?>
+                            </div>
+                        </div>
+                    <?php endwhile;?>
                 </div>
-            </div>
-            <?php endwhile;?>
-            <!--  -->
+            <?php endif;?>
         </div>
     </div>
-    </div>
 </section>
-<?php endif;?>
